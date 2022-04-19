61d25d31399103eded73b42548cbfc8fa7e27d38
Whiffles <68132950+whifflesdev@users.noreply.github.com>
2022-04-19 00:32:51 -0700

    Create practice.js
    practice for variables and types

---
+++ practice.js
@@ -0,0 +1,13 @@
+// = assigns the number to the variable
+// concat string with variable
+// then prints
+
+
+var myNumber = 4;
+  console.log("myNumber is equal to " + myNumber);
+
+var myString = "Hello, World!";
+  console.log("myString is equal to " + myString);
+
+var myBoolean = false;
+  console.log("myBoolean is equal to " + myBoolean);
